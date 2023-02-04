0090e7143b877ec3c4626d6f45077867605af2a8
CmD0_ <elouan112@gmail.com>
2023-02-04 12:42:00 -0500

    added database connection and query code

---
+++ src/database_operations.py
@@ -0,0 +1,27 @@
+import os
+from pymongo import MongoClient
+
+
+def get_database():
+    user = os.environ.get("mongoUser")
+    password = os.environ.get("mongoPass")
+    CONNECTION_STRING = f"mongodb+srv://{user}:{password}@cluster0.yy0jlun.mongodb.net/?retryWrites=true&w=majority"
+
+    client = MongoClient(CONNECTION_STRING)
+
+    return client.sample_mflix
+
+
+def get_collection(collection_name):
+    db = get_database()
+    return db[collection_name]
+
+
+def get_document(collection_name, query: dict):
+    collection = get_collection(collection_name)
+    return collection.find_one(query)
+
+
+def get_many_documents(collection_name, query: dict):
+    collection = get_collection(collection_name)
+    return collection.find(query)
--- src/main.py
+++ src/main.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import database_operations as db_ops
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
-
+    db = db_ops.get_database()
+    movies = db.movies
+    movie = movies.find_one()
+    title = movie["title"]
+    response = f"Hello, {title}!"
+    return response
